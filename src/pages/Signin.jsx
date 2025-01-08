9f8e745937a900f797211338fe1ed1d65d7c544d
Justine <paralejasjustine15@gmail.com>
2025-01-08 21:35:46 +0800

    Small fix.

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -15,7 +15,6 @@ const Signin = () => {
   return (
     <div className="flex flex-col justify-center items-center w-full h-full bg-primary text-xs p-4 rounded-md">
       <SigninForm />
-      <WebhookForm />
     </div>
   );
 };
