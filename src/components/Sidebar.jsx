2b181eb6b84b677ae265d51c527966351032153f
Justine <paralejasjustine15@gmail.com>
2025-01-17 01:27:17 +0800

    Test commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
+import useAxios from "../hooks/useAxios";
 import Button from "./ui/Button";
 
 const Sidebar = () => {
+  const { api, isAxiosReady } = useAxios();
+
+  useEffect(() => {
+    if (isAxiosReady) {
+      api.get("/output").then((response) => {
+        console.log(response.data);
+      });
+    }
+  }, [api, isAxiosReady]);
+
   return (
     <div className="flex flex-col min-w-[220px] h-full bg-primary rounded-md gap-2 p-4">
       <Button text="Repositories" />
