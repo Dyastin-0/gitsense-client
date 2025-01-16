ea1b9663b01d1160ed9f8b8db35fc8e5144219bb
Justine <paralejasjustine15@gmail.com>
2025-01-17 02:22:08 +0800

    UI fixes

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -3,14 +3,21 @@ import Separator from "./ui/Separator";
 const Event = ({ event }) => {
   return (
     <div className="flex flex-col p-2 gap-2 rounded-md text-xs text-primary-foreground">
-      <h1 className="text-xs font-semibold">{event.webhook}</h1>
-      <span className="text-secondary-foreground">{`Event ID: ${event.id}`}</span>
+      <div className="flex gap-1">
+        <h1 className="text-xs font-semibold">{event.webhook}</h1>
+        <span className="text-secondary-foreground">{`${event._id}`}</span>
+      </div>
       <Separator />
       <h2 className="text-secondary-foreground">Output</h2>
-      <span className="whitespace-pre-wrap">{event.stdout}</span>
+      <code className="whitespace-pre-wrap">{`${event.stdout}`}</code>
       <Separator />
-      <h2 className="text-secondary-foreground">Error</h2>
-      <span className="whitespace-pre-wrap">{event.stderr}</span>
+      {event.stderr && (
+        <>
+          <h2 className="text-secondary-foreground">Error</h2>
+          <code className="whitespace-pre-wrap">{`${event.stderr}`}</code>
+          <Separator />
+        </>
+      )}
     </div>
   );
 };
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,6 @@ const Sidebar = () => {
   return (
     <div className="flex flex-col min-w-[220px] h-full bg-primary rounded-md gap-2 p-3">
       <Button text="Repositories" onClick={() => setContent("repositories")} />
-      <Button text="Webhooks" />
       <Button text="Events" onClick={() => setContent("events")} />
     </div>
   );
